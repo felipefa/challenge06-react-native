d71e5d8dc48a94cf65e6735023545526e02f58cc
Felipe Araujo <devaraujofelipe@gmail.com>
2019-10-09 13:21:49 -0300

    Fix device IP for reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '10.10.1.66' })
+  const tron = Reactotron.configure({ host: '192.168.1.3' })
     .useReactNative()
     .connect();
 
